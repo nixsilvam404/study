b5905170940efcb706edd89cab336f3755cdd6af
Nix Silvam <nixsilvam@icloud.com>
2023-06-01 13:36:13 +0300

    fkn frst hmwrk

---
+++ hillel/01_hw.py
@@ -0,0 +1,7 @@
+x = int(input("Input the number: "))
+y = int(input("Input the sec number: "))
+
+print(x, y)
+print(f"The sum of numbers: {x+y}")
+print(f"The integer division of numbers: {x//y}")
+print(f"The remainder is: {x%y}")
