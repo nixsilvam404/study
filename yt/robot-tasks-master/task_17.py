d225edb05993f74a1747950e05c31ab30721b0a2
Nix Silvam <nixsilvam@icloud.com>
2023-06-18 18:09:57 +0300

    "robot" tasks

--- yt/robot-tasks-master/task_17.py
+++ yt/robot-tasks-master/task_17.py
@@ -5,7 +5,11 @@ from pyrob.api import *
 
 @task
 def task_8_27():
-    pass
+    while cell_is_filled() is False:
+        move_up()
+    move_right()
+    if cell_is_filled() is False:
+        move_left(2)
 
 
 if __name__ == '__main__':
