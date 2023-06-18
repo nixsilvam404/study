46675d23c9d986fcefe4e41c37367d98def6edf3
Nix Silvam <nixsilvam@icloud.com>
2023-06-18 13:49:14 +0300

    "robot" tasks

--- yt/robot-tasks-master/task_9.py
+++ yt/robot-tasks-master/task_9.py
@@ -5,7 +5,22 @@ from pyrob.api import *
 
 @task
 def task_8_2():
-    pass
+    while True:
+        if wall_is_beneath() and wall_is_above() and (wall_is_on_the_right() is False):
+            move_right()
+        elif wall_is_beneath() and (wall_is_above() is False) and (wall_is_on_the_right() is False):
+            fill_cell()
+            move_right()
+        elif wall_is_beneath() and (wall_is_above() is False) and wall_is_on_the_right():
+            fill_cell()
+            break
+        elif wall_is_beneath() and wall_is_above() and wall_is_on_the_right():
+            break
+
+
+
+
+
 
 
 if __name__ == '__main__':
