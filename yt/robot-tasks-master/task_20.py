19bd035a1c0c390418034abb2060f4998044a6b0
Nix Silvam <nixsilvam@icloud.com>
2023-06-19 15:51:23 +0300

    "robot" tasks

--- yt/robot-tasks-master/task_20.py
+++ yt/robot-tasks-master/task_20.py
@@ -5,7 +5,20 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
+    move_right()
+    for i in range(6):
+        while wall_is_on_the_right() is False:
+            fill_cell()
+            move_right()
+        if wall_is_on_the_right():
+            move_down()
+            move_left()
+        while wall_is_on_the_left() is False:
+            fill_cell()
+            move_left()
+        if wall_is_on_the_left():
+            move_down()
+            move_right()
 
 
 if __name__ == '__main__':
--- yt/robot-tasks-master/task_21.py
+++ yt/robot-tasks-master/task_21.py
@@ -5,7 +5,26 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
+    n = 13
+    move_down()
+    move_right()
+    while n > 1:
+        for i in range(n):
+            fill_cell()
+            move_right()
+            move_down()
+        n -= 1
+        move_left(2)
+        move_up()
+        for i in range(n):
+            fill_cell()
+            move_left()
+            move_up()
+        n -= 1
+        move_down(2)
+        move_right()
+    fill_cell()
+    move_down()
 
 
 if __name__ == '__main__':
