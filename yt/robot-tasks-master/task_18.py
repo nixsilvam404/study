c3a58effb187efa51ac53d887e09a0115d0fa6fd
Nix Silvam <nixsilvam@icloud.com>
2023-06-19 10:56:11 +0300

    "robot" tasks

--- yt/robot-tasks-master/task_18.py
+++ yt/robot-tasks-master/task_18.py
@@ -5,7 +5,18 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
+    while wall_is_above() and wall_is_beneath() and wall_is_on_the_right() is False:
+        move_right()
+        if wall_is_on_the_right():
+            while wall_is_above() and wall_is_beneath():
+                move_left()
+        elif wall_is_on_the_left():
+            while wall_is_above() and wall_is_beneath():
+                move_right()
+    while wall_is_above() is False:
+        move_up()
+    while wall_is_on_the_left() is False:
+        move_left()
 
 
 if __name__ == '__main__':
--- yt/robot-tasks-master/task_19.py
+++ yt/robot-tasks-master/task_19.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    pass
+    while wall_is_above() and wall_is_beneath() and wall_is_on_the_left() is False:
+        move_left()
+        while wall_is_above() is False and wall_is_beneath() is False:
+            move_up()
+    while wall_is_above() and wall_is_beneath() and wall_is_on_the_right() is False:
+        move_right()
+        while wall_is_above() is False and wall_is_beneath() is False:
+            move_up()
+            while wall_is_on_the_left() is False:
+                move_left()
 
 
 if __name__ == '__main__':
