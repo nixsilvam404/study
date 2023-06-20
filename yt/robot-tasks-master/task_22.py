4dc66c61981b872bd8af7d7457a36f2e4df2002a
Nix Silvam <nixsilvam@icloud.com>
2023-06-20 12:08:08 +0300

    "robot" tasks http://judge.mipt.ru/mipt_cs_on_python3/labs/lab2.html

--- yt/robot-tasks-master/task_22.py
+++ yt/robot-tasks-master/task_22.py
@@ -5,7 +5,34 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+    while True:
+        while wall_is_on_the_right() is False and wall_is_beneath() is False:
+            fill_cell()
+            move_right()
+        if wall_is_on_the_right() and wall_is_beneath() is False:
+            fill_cell()
+            move_down()
+        while wall_is_on_the_left() is False and wall_is_beneath() is False:
+            fill_cell()
+            move_left()
+        if wall_is_on_the_left() and wall_is_beneath() is False:
+            fill_cell()
+            move_down()
+        while wall_is_on_the_right() is False and wall_is_beneath() and cell_is_filled() is False:
+            fill_cell()
+            move_right()
+        while wall_is_on_the_left() is False and wall_is_beneath() and cell_is_filled() is False:
+            fill_cell()
+            move_left()
+        while wall_is_on_the_left() is False and wall_is_beneath() and cell_is_filled():
+            move_left()
+        if wall_is_on_the_left() and wall_is_beneath() and cell_is_filled():
+            break
+        if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
+            fill_cell()
+            break
+        else:
+            break
 
 
 if __name__ == '__main__':
--- yt/robot-tasks-master/task_23.py
+++ yt/robot-tasks-master/task_23.py
@@ -5,7 +5,35 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_6_6():
-    pass
+    move_right()
+    while wall_is_on_the_right() is False and wall_is_beneath():
+        if wall_is_above() is False and wall_is_beneath():
+            move_up()
+            fill_cell()
+            while wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above() is False:
+                move_up()
+                fill_cell()
+        if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above():
+            move_down()
+        while cell_is_filled():
+            move_down()
+        move_right()
+    # last tunel begin
+    if wall_is_on_the_right() and wall_is_above() is False and wall_is_beneath():
+        move_up()
+        fill_cell()
+    while wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above() is False:
+        move_up()
+        fill_cell()
+    if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above():
+        move_down()
+    while cell_is_filled():
+        move_down()
+    # last tunel end
+    if wall_is_on_the_right() and wall_is_beneath():
+        move_left()
+    while wall_is_beneath():
+        move_left()
 
 
 if __name__ == '__main__':
