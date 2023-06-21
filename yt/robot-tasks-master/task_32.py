46a6c43329c414441294b7ee77b6b67e27d656ee
Nix Silvam <nixsilvam@icloud.com>
2023-06-21 21:11:16 +0300

    "robot" tasks http://judge.mipt.ru/mipt_cs_on_python3/labs/lab2.html

--- yt/robot-tasks-master/task_32.py
+++ yt/robot-tasks-master/task_32.py
@@ -5,7 +5,30 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    pass
+    x = 0
+    while not wall_is_on_the_right():
+        if cell_is_filled():
+            x += 1
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()
+        move_right()
+        if not wall_is_above() and wall_is_beneath():
+            move_up()
+            while wall_is_on_the_right() and wall_is_on_the_left() and not wall_is_above():
+                if cell_is_filled():
+                    x += 1
+                else:
+                    fill_cell()
+                move_up()
+        if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above():
+            if cell_is_filled():
+                x += 1
+            else:
+                fill_cell()
+            move_down()
+        while cell_is_filled():
+            move_down()
+    mov('ax', x)
 
 
 if __name__ == '__main__':
