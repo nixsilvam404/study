acebe4d88db22d5b929e7f36047f07ca8e9b8f5b
Nix Silvam <nixsilvam@icloud.com>
2023-06-21 21:36:57 +0300

    "robot" tasks http://judge.mipt.ru/mipt_cs_on_python3/labs/lab2.html

--- yt/robot-tasks-master/task_18.py
+++ yt/robot-tasks-master/task_18.py
@@ -5,14 +5,10 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    while wall_is_above() and wall_is_beneath() and wall_is_on_the_right() is False:
+    while not wall_is_on_the_right() and wall_is_above() and wall_is_beneath():
         move_right()
-        if wall_is_on_the_right():
-            while wall_is_above() and wall_is_beneath():
-                move_left()
-        elif wall_is_on_the_left():
-            while wall_is_above() and wall_is_beneath():
-                move_right()
+    while not wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
+        move_left()
     while wall_is_above() is False:
         move_up()
     while wall_is_on_the_left() is False:
--- yt/robot-tasks-master/task_22.py
+++ yt/robot-tasks-master/task_22.py
@@ -6,32 +6,30 @@ from pyrob.api import *
 @task
 def task_5_10():
     while True:
-        while wall_is_on_the_right() is False and wall_is_beneath() is False:
+        while not wall_is_on_the_right() and not wall_is_beneath():
             fill_cell()
             move_right()
-        if wall_is_on_the_right() and wall_is_beneath() is False:
+        if wall_is_on_the_right() and not wall_is_beneath():
             fill_cell()
             move_down()
-        while wall_is_on_the_left() is False and wall_is_beneath() is False:
+        while not wall_is_on_the_left() and not wall_is_beneath():
             fill_cell()
             move_left()
-        if wall_is_on_the_left() and wall_is_beneath() is False:
+        if wall_is_on_the_left() and not wall_is_beneath():
             fill_cell()
             move_down()
-        while wall_is_on_the_right() is False and wall_is_beneath() and cell_is_filled() is False:
+        while not wall_is_on_the_right() and wall_is_beneath() and not cell_is_filled():
             fill_cell()
             move_right()
-        while wall_is_on_the_left() is False and wall_is_beneath() and cell_is_filled() is False:
+        while not wall_is_on_the_left() and wall_is_beneath() and not cell_is_filled():
             fill_cell()
             move_left()
-        while wall_is_on_the_left() is False and wall_is_beneath() and cell_is_filled():
+        while not wall_is_on_the_left() and wall_is_beneath() and cell_is_filled():
             move_left()
         if wall_is_on_the_left() and wall_is_beneath() and cell_is_filled():
             break
         if wall_is_on_the_right() and wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
             fill_cell()
-            break
-        else:
             break
 
 
