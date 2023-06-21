b91605be06c8b946597d880764c98210dad9bd77
Nix Silvam <nixsilvam@icloud.com>
2023-06-21 19:41:06 +0300

    "robot" tasks http://judge.mipt.ru/mipt_cs_on_python3/labs/lab2.html

--- yt/robot-tasks-master/task_30.py
+++ yt/robot-tasks-master/task_30.py
@@ -3,9 +3,48 @@
 from pyrob.api import *
 
 
+def move(n):
+    for i in range(n):
+        move_right()
+        fill_cell()
+    move_right()
+    for i in range(n):
+        move_down()
+        fill_cell()
+    move_down()
+    for i in range(n):
+        move_left()
+        fill_cell()
+    move_left()
+    for i in range(n):
+        move_up()
+        fill_cell()
+    move_up()
+
+
+def count():
+    nn = 1
+    while not wall_is_on_the_right():
+        move_right()
+        nn += 1
+    while not wall_is_on_the_left():
+        move_left()
+    return nn - 2
+
+
 @task(delay=0.01)
 def task_9_3():
-    pass
+    n = count()
+    if n > 0:
+        for i in range(n):
+            move(n)
+            move_down()
+            move_right()
+            n -= 2
+    while not wall_is_beneath():
+        move_down()
+    while not wall_is_on_the_left():
+        move_left()
 
 
 if __name__ == '__main__':
--- yt/robot-tasks-master/task_31.py
+++ yt/robot-tasks-master/task_31.py
@@ -5,7 +5,22 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+    x = 0
+    while True:
+        while not wall_is_on_the_right():
+            move_right()
+        while not wall_is_beneath():
+            move_down()
+        while wall_is_beneath():
+            move_left()
+            if not wall_is_beneath():
+                move_down()
+                x = 1
+        if x == 0:
+            break
+
+
+
 
 
 if __name__ == '__main__':
