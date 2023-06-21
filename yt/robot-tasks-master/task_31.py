5916683dc18b1525de1952af84ed2e4466dc2524
Nix Silvam <nixsilvam@icloud.com>
2023-06-21 20:32:23 +0300

    "robot" tasks http://judge.mipt.ru/mipt_cs_on_python3/labs/lab2.html

--- yt/robot-tasks-master/task_31.py
+++ yt/robot-tasks-master/task_31.py
@@ -3,24 +3,31 @@
 from pyrob.api import *
 
 
+def count():
+    n = 1
+    while not wall_is_on_the_left():
+        move_left()
+    while not wall_is_on_the_right():
+        move_right()
+        n += 1
+    return n
+
+
 @task(delay=0.01)
 def task_8_30():
-    x = 0
-    while True:
+    n = count()
+    x = 1
+    while x != n:
         while not wall_is_on_the_right():
             move_right()
         while not wall_is_beneath():
             move_down()
-        while wall_is_beneath():
+        while wall_is_beneath() and x != n:
             move_left()
-            if not wall_is_beneath():
-                move_down()
-                x = 1
-        if x == 0:
-            break
-
-
-
+            x += 1
+        while not wall_is_beneath():
+            move_down()
+            x = 1
 
 
 if __name__ == '__main__':
